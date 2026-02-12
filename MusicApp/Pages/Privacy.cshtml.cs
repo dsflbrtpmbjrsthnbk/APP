cc5bacbe278a8a7bd3ee449a3b0c1c38ad4da63c
Karimov_a7 <abdukarimov14052005@gmail.com>
2026-02-13 02:39:13 +0500

    APP

---
+++ MusicApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
